5cf9afff4b87f2e4cdc51ccb0d5411a5cf7756e0
YadnikBangale <bangaleyj@rknec.edu>
2026-03-11 00:25:32 +0530

    Updated the main.py for day 2 task

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from model import load_dataset
+from pydantic import BaseModel
 
 app = FastAPI()
 
 data = None
+
+class IdeaRequest(BaseModel):
+    idea : str
 
 @app.on_event("startup")
 def startup_event():
@@ -17,5 +21,9 @@ def home():
     return {"message": "Idea Novelty Analyzer API running"}
 
 @app.post("/analyze")
-def analyze_idea(idea: str):
-    return {"message": "Idea received", "idea": idea}
+def analyze_idea(request: IdeaRequest):
+    idea_text = request.idea
+
+    print("Data Recieved", idea_text)
+    return {"message": "Idea received", 
+            "idea": idea_text}
