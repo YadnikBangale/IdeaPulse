ea0a7d53b9bb426053bc65fc930decd1fb1588a1
GouriRajkarne <rajkarnegu@rknec.edu>
2026-03-12 20:56:26 +0530

    Day 2: Added text embedding generation using SentenceTransformers

--- Backend/main.py
+++ Backend/main.py
@@ -1,29 +1,42 @@
 from fastapi import FastAPI
-from model import load_dataset
+from model import load_dataset, generate_embeddings
 from pydantic import BaseModel
 
 app = FastAPI()
 
 data = None
+embeddings = None
+
 
 class IdeaRequest(BaseModel):
-    idea : str
+    idea: str
+
 
 @app.on_event("startup")
 def startup_event():
-    global data
+    global data, embeddings
+
     print("Loading dataset...")
     data = load_dataset()
     print("Dataset shape:", data.shape)
+
+    print("Generating embeddings...")
+    embeddings = generate_embeddings(data)
+    print("Embeddings shape:", embeddings.shape)
+
 
 @app.get("/")
 def home():
     return {"message": "Idea Novelty Analyzer API running"}
 
+
 @app.post("/analyze")
 def analyze_idea(request: IdeaRequest):
     idea_text = request.idea
 
-    print("Data Recieved", idea_text)
-    return {"message": "Idea received", 
-            "idea": idea_text}
+    print("Data Received:", idea_text)
+
+    return {
+        "message": "Idea received",
+        "idea": idea_text
+    }
--- Backend/model.py
+++ Backend/model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
 
 def load_dataset():
     path = "../Dataset/idea_novelty_starter_dataset.csv"
@@ -11,3 +13,22 @@ def load_dataset():
     print(data.head())
 
     return data
+
+
+def generate_embeddings(data):
+
+    print("Loading embedding model...")
+
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    print("Generating embeddings from descriptions...")
+
+    embeddings = model.encode(data["description"].tolist())
+
+    print("Embeddings generated!")
+
+    np.save("../Dataset/idea_embeddings.npy", embeddings)
+
+    print("Embeddings saved as idea_embeddings.npy")
+
+    return embeddings
